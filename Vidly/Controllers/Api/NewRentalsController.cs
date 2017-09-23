6ca12feec900b7a9630bb3ae27a58f517dbb80d4
Allan SuRu <https://github.com/allansuru/MoviesMVC.git>
2017-09-23 19:00:13 -0300

    Adicionando um novo modelo "RENTAL(Aluguel)", depois crio o dto dele pra trabalhar de uma forma mais leve, adiciono o dbSet, adicionei tbm na classe movie uma especie de contador pra eu validar se o movie está disponível. Depois de modificar a classe Movie e add no Context a classe Rental, eu gerei um add database/UpdateDatabase via Migration. e pra finalizar, criei o API do Rental

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
+
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+            foreach (var movie in movies)
+            {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Filme não está avaliado para ser alugado");
+
+                movie.NumberAvailable--;
+
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+
+                };
+
+                _context.SaveChanges();
+
+            }
+
+            return Ok();
+        }
+    }
+}
