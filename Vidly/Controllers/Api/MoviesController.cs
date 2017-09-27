fccf6f87d065759dd50cada9d2ab356189d26a47
Allan SuRu <https://github.com/allansuru/MoviesMVC.git>
2017-09-27 20:59:39 -0300

    - Corrigindo estilo do typeahead - Fazendo ajustes nas APis para funcionar o aluguel corretamente

--- Vidly/Controllers/Api/MoviesController.cs
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -18,12 +18,18 @@ namespace Vidly.Controllers.Api
             _context = new ApplicationDbContext();
         }
 
-        public IEnumerable<MovieDto> GetMovies()
-        {
-             return _context.Movies
-                 .Include(m => m.Genre)
-                 .ToList()
-                 .Select(Mapper.Map<Movie, MovieDto>);
+        public IEnumerable<MovieDto> GetMovies(string query = null)
+         {
+            var moviesQuery = _context.Movies
+                   .Include(m => m.Genre)
+                   .Where(m => m.NumberAvailable > 0);
+
+            if (!String.IsNullOrWhiteSpace(query))
+                moviesQuery = moviesQuery.Where(m => m.Name.Contains(query));
+
+            return moviesQuery
+                .ToList()
+                .Select(Mapper.Map<Movie, MovieDto>);
         }
 
         public IHttpActionResult GetMovie(int id)
